6853308bed66c85348c1a4efe0a175cd0fe55135
takumiyanagihara <takumiyanagihara@forest.com>
2025-06-13 11:18:16 +0900

    Vueプロジェクト統合：.git削除済み

---
+++ back_test/src/test/java/jp/co/feeps/BackTestApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.feeps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
